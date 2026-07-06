38e31efac2475cfe23016f1d937ffb9d34f29748
leozam02 <ltzambra@espol.edu.ec>
2026-07-06 14:46:34 -0500

    Adding scenarios

--- features/inventory.feature
+++ features/inventory.feature
@@ -4,3 +4,9 @@ Feature: Inventory Manager
     Given the inventory is empty
     When the user adds a product "Coffee"
     Then the inventory should contain "Coffee"
+
+
+  Scenario: Search for a product that does not exist
+    Given the inventory is empty
+    When the user searches for the product "Mouse"
+    Then the output should be "Product 'Mouse' not found."
--- features/steps/inventory_steps.py
+++ features/steps/inventory_steps.py
@@ -19,3 +19,16 @@ def step_impl_add_product(context, name):
 @then('the inventory should contain "{name}"')
 def step_impl_check_contains(context, name):
     assert name in context.inventory.products, f'Product "{name}" not found in the inventory'
+
+
+@when('the user searches for the product "{product}"')
+def step_impl_search_product(context, product):
+    # Llamamos a tu función search_product y guardamos el éxito y el mensaje
+    context.success, context.output = context.inventory.search_product(product)
+
+@then('the output should be "{message}"')
+def step_impl_check_output(context, message):
+    # Validamos que tu función haya retornado False
+    assert context.success is False, "Se esperaba que la búsqueda fallara"
+    # Validamos que el texto coincida exactamente con lo esperado
+    assert context.output == message, f'Expected "{message}" but got "{context.output}"'
